fbdcf87a4f8ea5576c1a9b2026c4a5ea06a8a9ad
SaiNikhil <46104400+palukurisainikhil@users.noreply.github.com>
2024-04-03 10:00:14 +0530

    Add files via upload

---
+++ src/main/java/com/example/reloadablepropertiesclient/ReloadablePropertiesClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.reloadablepropertiesclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReloadablePropertiesClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReloadablePropertiesClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reloadablepropertiesclient/ReloadablePropertiesClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reloadablepropertiesclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReloadablePropertiesClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
